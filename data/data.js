a8b298668ecb21617be6460f8ee16221b7c9afc8
Zrazhevskii <vbn-134@yandex.ru>
2023-11-10 12:22:47 +0300

    создание структуры

---
+++ data/data.js
@@ -0,0 +1,11 @@
+const { randomUUID } = require('crypto')
+
+const notes = [
+    { id: '1', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, in!' },
+    { id: '2', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, in!' },
+  ];
+  
+  console.log(randomUUID());
+
+
+  export default notes;
---
+++ notes/nodeAdd.js
@@ -0,0 +1,1 @@
+const { randomUUID } = require('crypto')
