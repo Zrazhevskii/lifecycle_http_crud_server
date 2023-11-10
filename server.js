70442ebbc7b6b7fa6884101f1f4a701f6fc7fbd0
Zrazhevskii <vbn-134@yandex.ru>
2023-11-10 15:36:49 +0300

    коррекция, удаление лишнего

--- server.js
+++ server.js
@@ -1,37 +1,12 @@
-// const http = require('http');
 const koaBody = require('koa-body').default;
 const cors = require('koa2-cors');
 const noutes = require('./data/data');
-// const Router = require('koa-router');
-// const router = new Router();
 const { randomUUID } = require('crypto');
 const PORT = 3000;
 
 const Koa = require('koa');
 const router = require('./routes');
 const app = new Koa();
-
-// router
-//     .get('/', (ctx, next) => {
-//         ctx.body = 'Всем приветы!';
-//         next()
-//     })
-//     .get('/noutes', (ctx) => {
-//         ctx.body = noutes;
-//     })
-//     .post('/noutes/', async (ctx) => {
-//         noutes.push({ ...ctx.request.body, id: randomUUID() });
-//         ctx.response.status = 204;
-//     })
-//     .delete('/noutes/:id', async (ctx) => {
-//         const noteID = ctx.params.id;
-//         const index = noutes.findIndex((o) => o.id === noteID);
-//         if (index !== -1) {
-//             noutes.splice(index, 1);
-//         }
-//         ctx.response.status = 204;
-    
-// });
 
 app.use(
     koaBody({
